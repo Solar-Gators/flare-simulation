86b09968bc0cd95888ecdcf1cc17c16c08b418ff
Anthony Zheng <anthonyzheng124@gmail.com>
2025-10-09 19:47:10 -0400

    working on iterating through track and calculating coast distance prior to curve

--- backend/flare_simulation.go
+++ backend/flare_simulation.go
@@ -8,25 +8,43 @@ func main() {
 	/*solarYield := 0.0
 	maxSpeed := 50.0
 	maxGforce := 0.5 */
-	battCharge := 100.0 
+	// battCharge := 100.0 
 	//time 19.22 for day 1
 	straight_path := Segment{Length: 100}
 	straight_path1 := Segment{Length: 100}
 	straight_path2 := Segment{Length: 100}
 	curved_path := Segment{Radius: 90, Angle: 90}
+	straight_path3 := Segment{Length: 100}
+	curved_path2 := Segment{Radius: 90, Angle: 90}
 
-	race_track := Track{Segments: []Segment{straight_path, straight_path1, straight_path2, curved_path}}
+	race_track := Track{Segments: []Segment{straight_path, straight_path1, straight_path2, curved_path, straight_path3, curved_path2}}
 
 	totalLength := 0.0
 	for i := 0; i < len(race_track.Segments); i++ {
 		totalLength += race_track.Segments[i].getArcLength()
 	}
 
-	for i := 1.0; i <= totalLength; i++ {
-		fmt.Print(i)
-		fmt.Print(": ")
-		fmt.Print(battCharge / totalLength)
-		fmt.Println()
+	for i := 0; i < len(race_track.Segments); i++ {
+		if ((i+1) == len(race_track.Segments)-1){
+			break
+		}
+		speed := 50.0
+		upcomingCurve := race_track.Segments[i+1]
+		aDrag := 0.21
+		rRes := 0.0015
+		gravity := 9.81
+		gmax := 0.8
+		mass := 285.0
+		fArea := 0.456 //frontal area
+		rho := 1.225 //air density
+
+		if (upcomingCurve.Radius > 0) {
+			DistanceToCoast := calcCoastDistance(speed, upcomingCurve, aDrag, rRes, gravity, gmax, mass, fArea, rho)
+			if (DistanceToCoast > race_track.Segments[i].Length){
+				fmt.Println("NOT ENOUGH TRACK TO SLOW DOWN. BRAKE!!", "I NEED ", DistanceToCoast)
+			}
+			fmt.Println("IM SLOWING IT DOWN TWIN", "I NEED ", DistanceToCoast)
+		}
 	}
 
 }
--- backend/gforce.go
+++ backend/gforce.go
@@ -1,22 +1,15 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
-// Calculates the g force of current turn/curve
+// Calculates the max speed of upcoming curve
 func CalcGforce(segments Segment, gravity float64, gmax float64) float64 {
 	radius := segments.Radius
 	maxVelocity := math.Sqrt(gmax * radius * gravity)
 	return maxVelocity
 }
-
-// How much energy is used given a constant acceleration after a curve
-func curveAccelEnergy(coastSpeed float64, currentSpeed float64, acceleration float64, distance float64) float64 {
-	var mass float64 = 250
-	energyUsed := 0.5 * mass * ((math.Pow(coastSpeed, 2)) - (math.Pow(currentSpeed, 2)))
-
-	return energyUsed
-}
-
 
 //determines distance to let off accelerator before a curve to hit target speed
 //taking in the same params as CalcGForce to be passed into it
@@ -37,8 +30,7 @@ func calcCoastDistance(
 	//rho: air density
 	
 	//target speed obtained from CalcGforce func
-	targetSpeed := CalcGforce(segment, gravity, gmax)
-
+	targetSpeed := CalcGforce(segment, gravity, gmax) //m/s
 	//squaring initial and end speeds
 	initSpeedSquared := math.Pow(currentSpeed, 2)
 	endSpeedSquared := math.Pow(targetSpeed, 2)
@@ -46,16 +38,23 @@ func calcCoastDistance(
 	//quotient of coasting distance formula
 	startTerm := (((rho*aDrag*fArea)/(2*mass)*initSpeedSquared + (rRes*gravity)))
 	endTerm := (((rho*aDrag*fArea)/(2*mass)*endSpeedSquared + (rRes*gravity)))
-
-	lnTerm := math.Log(startTerm/endTerm)
 	
+	lnTerm := math.Log(startTerm/endTerm)
+
 	coastDistance := ((mass/(rho*aDrag*fArea))*lnTerm)
 	return coastDistance
 }
-
 
 // How much energy is saved by letting off the gas before a curve
 func coastConservation(coastEnergy, bottomEnergy, distance float64) float64 {
 	energyConserved := distance * (coastEnergy - bottomEnergy)
 	return energyConserved
+}
+
+// How much energy is used given a constant acceleration after a curve
+func curveAccelEnergy(coastSpeed float64, currentSpeed float64, acceleration float64, distance float64) float64 {
+	var mass float64 = 250
+	energyUsed := 0.5 * mass * ((math.Pow(coastSpeed, 2)) - (math.Pow(currentSpeed, 2)))
+
+	return energyUsed
 }
